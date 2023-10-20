6e1ab496dc016b25bdf4b6e22b2ed3e02dc921b2
Carlos Garcia Martin <cagarci2@c3r2s4.42malaga.com>
2023-10-20 14:20:06 +0200

    add

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: cagarci2 <cagarci2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 13:28:33 by cagarci2          #+#    #+#             */
-/*   Updated: 2023/10/20 11:23:07 by cagarci2         ###   ########.fr       */
+/*   Updated: 2023/10/20 14:07:51 by cagarci2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,16 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int	i;
-	int	len;
+	char	*str;
+	int		i;
+	int		len;
 
 	len = ft_strlen(s1);
 	i = 0;
-while (*s1 ||)
+	while (s1[i] && ft_strchr(set, s1[i]))
+		i++;
+	while (i < len && ft_strchr(set, s1[len -1]))
+		len--;
+	str = ft_substr(s1, i, len - i);
+	return (str);
 }
-
